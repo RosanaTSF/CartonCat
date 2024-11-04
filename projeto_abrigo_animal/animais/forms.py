2e2339429cb73679fadb6bca4cca5c95ffdf3d3a
Rosana TSF <rosanatsf@gmail.com>
2024-11-04 18:09:22 -0300

    Add Register e templatetags

--- projeto_abrigo_animal/animais/forms.py
+++ projeto_abrigo_animal/animais/forms.py
@@ -1,4 +1,3 @@
-# forms.py
 from django import forms
 from .models import Animal
 
@@ -6,5 +5,6 @@ class AnimalForm(forms.ModelForm):
     class Meta:
         model = Animal
         fields = ['status', 'nome', 'sexo', 'cor', 'detalhes_medicos', 'foto_animal']
-        
-   
+        widgets = {
+            'nome': forms.TextInput(attrs={'placeholder': 'Escolha um nome de usuário', 'class': 'form-control'}),
+        }
